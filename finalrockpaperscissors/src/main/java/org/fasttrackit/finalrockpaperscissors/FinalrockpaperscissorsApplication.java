0a66eb872f21dfd65a8d940a369827cd49ba1765
Calin Andreca <calin.andreca@gmail.com>
2023-08-04 16:59:10 +0300

    Initial commit

---
+++ finalrockpaperscissors/src/main/java/org/fasttrackit/finalrockpaperscissors/FinalrockpaperscissorsApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.finalrockpaperscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalrockpaperscissorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalrockpaperscissorsApplication.class, args);
+	}
+
+}
